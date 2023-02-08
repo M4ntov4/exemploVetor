15568a2fc348081a8634948ad6f1d31108d88aa9
João Vitor Delaia <115042912+Mantovinha@users.noreply.github.com>
2023-02-07 23:27:11 -0300

    Add files via upload

---
+++ vetoresExemplo/vetoresExemplo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vetoresExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valor = new int[5];// basico pra declarar um vetor
+
+            int[] valor2; // basico para um usuario definir um tamanho parte 1
+            int valor3;
+
+            Console.WriteLine("insira o tamanho do vetor: ");
+            valor3 = int.Parse(Console.ReadLine());
+
+            valor2 = new int[valor3]; // basico para um usuario definir um tamanho parte 2
+
+
+        }
+    }
+}
